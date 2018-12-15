20885b9f1179272f71ed22fb246d0a6687cb656e
Ilse van Beelen <ilsevanbeelen@live.nl>
2018-12-15 01:43:35 +0100

    laatste veranderinge

--- Data_visualisation.R
+++ Data_visualisation.R
@@ -24,17 +24,20 @@ rm(list = ls()) # empty work space
 Data <- read.csv("1_clean_data/voting_and_demographics.csv",
                  stringsAsFactors=F, header = T)
 
-Data <- read.csv("1_clean_data/Cleandata_all_variables_2018-12-14.csv",
-                 stringsAsFactors=F, header = T)
+#Data <- read.csv("1_clean_data/Cleandata_all_variables_2018-12-14.csv",
+#                 stringsAsFactors=F, header = T)
 
 # Rename and remove columns
+# Do not put in report
 Data <- Data[,-15]
 colnames(Data) <- c("Muni", "VVD","CDA","PVV", "D66", "SP", "GL","PvdA","CU","50PLUS","PvdD","SGP","FvD","DENK", "Urban_index", "High_edu_perc", "Mean_income", "Dutch_perc", "West_perc", "Non_west_perc")
+
 
 # Non_west_perc is not linear. Therefore, we create a dummy variable with 3 levels
 # Level 1: x < 5%
 # Level 2: 5 <= x < 10%
 # Level 3: x >= 10%
+# Explain this in report
 Data$Non_west <- ifelse(Data$Non_west_perc < 0.05, 1, NA)
 Data$Non_west <- ifelse(Data$Non_west_perc >= 0.05 
                            & Data$Non_west_perc < 0.1, 2, Data$Non_west)
@@ -80,9 +83,7 @@ write.table(Dat2, file = paste("1_clean_data/Cleandata_GL_CDA",
 
 #### Demographics of data ####
 #Dat2$Non_west <- as.factor(Dat2$Non_west)
-str(Dat2)
 summary(Dat2) # zet in verslag & presentatie. Leg uit of variabele numeriek/factor is
-
 
 dens = ggplot(melt(Dat2), aes(x = value)) + 
   facet_wrap(~ variable, scales = "free", ncol = 2) + 
@@ -93,27 +94,17 @@ dens = ggplot(melt(Dat2), aes(x = value)) +
 plot(dens) # zet in verslag: normaal verdeling, density by CDA/GL/Perc 60plus/High edu
 # is boven de 1. Dit komt omdat de x-as kleine waarden heeft. Area onder curve somt tot 1
 
-#### GGplot set up ####
-# Not used at the moment, can be added as a layer to the plot
-theme_setup =  theme(axis.text=element_text(size=20),
-                     axis.title=element_text(size=22),
-                     plot.title = element_text(size = 22),
-                     strip.text = element_text(size = 20),
-                     legend.position="bottom",
-                     legend.text=element_text(size=20),
-                     legend.title=element_blank(),     
-                     panel.background = element_rect(fill = "lightgrey",
-                                                     colour = "white", size = 4))
 
 
 #### Correlation ####
 # Correlation and p-value matrices
+# Do not put in report
 corrlist <- rcorr(as.matrix(Dat_cda[,-1]), type="pearson")
 pvalues = data.frame(corrlist[["P"]])
 correlation = round(data.frame(corrlist[["r"]]), digits = 3)
 
 # Heatmap of the correlations
-# Zet in verslag: leg correlaties uit of we dat verwachten
+# Zet in verslag: leg correlaties uit of we dat verwachten/hoe te verklaren
 heatmap = ggcorr(Dat2[-1], 
                  low = "darkblue", mid = "lightyellow", high = "red",
                  label = T, label_size = 2.5, label_round = 3,   
@@ -125,6 +116,7 @@ plot(heatmap)
 
 
 #### Boxplots ####
+# For report: explain how to interpretate a boxplot
 # Plot Probability CDA votes VS Non-western residents
 p1 <- ggplot(Dat2, aes(x = Non_west, y = CDA, fill = Non_west)) + 
        geom_boxplot(outlier.colour="black", 
@@ -175,7 +167,8 @@ plot(p2a)
 
 
 #### Linear regression ####
-# Do not put R-squared function in report !
+# Report: explain what is on the x and y-axis
+# We can report the R-squared in the report (but we do not need to show this function)
 rsq <- function (x, y) cor(x, y) ^ 2
 rsq(Dat2$CDA, Dat2$Mean_income) # Calculate R-squared
 
@@ -195,7 +188,7 @@ p3 <- ggplot(Dat2, aes(x=Mean_income, y=CDA)) +
   #annotate("text", x=38, y=0.38, label = "correlation == -0.27", parse=T) + 
   #annotate("text", x=38, y=0.35, label = "p-value < 0.001", parse=T)
 
-plot(p3) # a few municipalities could be outliers -> possible high cooks dist?
+plot(p3) 
 
 
 p4 <- ggplot(Dat2, aes(x=Mean_income, y=High_edu_perc)) + 
@@ -261,7 +254,7 @@ p7 <- ggplot(Dat2, aes(x=Urban_index, y=GL)) +
 
 plot(p7)
 
-# Higly educted residents and GL votes
+# Highly educted residents and GL votes
 p8 <- ggplot(Dat2, aes(x=High_edu_perc, y=GL)) + 
   geom_point() + 
   geom_smooth(method=lm, se = T) + # standard error = True
